f3a3f43d74a38e78557b2b0b490f6b93ef074f8f
pietrobertino <pietrobertino20@gmail.com>
2026-09-22 20:31:12 +0200

    create basic page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import HomePage from "./pages/HomePage"
 
 function App() {
 
   return (
     <>
-      <h1 className="text-danger">Portfolio</h1>
+      <HomePage />
     </>
   )
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import AppHeader from "../components/AppHeader"
+import AppFooter from "../components/AppFooter"
+
+export default function HomePage() {
+    return (
+        <>
+            <AppHeader />
+            <main>
+                Main
+            </main>
+            <AppFooter />
+        </>
+    )
+}
